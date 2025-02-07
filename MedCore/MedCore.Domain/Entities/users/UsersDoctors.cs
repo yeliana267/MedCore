1799b9d4a35bb9a84f40302cb8f7834687f838fc
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-07 12:51:05 -0400

    a

---
+++ MedCore/MedCore.Domain/Entities/users/UsersDoctors.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace MedCore.Domain.Entities.users
+{
+    public sealed class UsersDoctors
+    {
+        public int DoctorID { get; set; }
+        public short SpecialtyID { get; set; }
+        public string LicenseNumber { get; set; }
+        public int PhoneNumber { get; set; }
+        public int YearsOfExperience { get; set; }
+        public string Education { get; set; }
+        public string? Bio { get; set; }
+        public decimal? ConsultationFee { get; set; }
+        public string? ClinicAddress { get; set; }
+        public short? AvailabilityModeId { get; set; }
+        public DateTime LicenseExpirationDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
