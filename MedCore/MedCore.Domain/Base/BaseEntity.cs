f4b09f7ca2412f542d842b5505468b54a17ea129
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-07 00:25:14 -0400

    Add project files.

---
+++ MedCore/MedCore.Domain/Base/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MedCore.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity() { 
+        this.CreatedAt = DateTime.Now;
+        }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
