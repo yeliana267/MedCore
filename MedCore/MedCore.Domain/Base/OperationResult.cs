211d20993db74de299e68465364e0bb00b2bae73
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-08 01:04:02 -0400

    Creacion de interface y operation result

---
+++ MedCore/MedCore.Domain/Base/OperationResult.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace MedCore.Domain.Base
+{
+    public class OperationResult
+    {
+        public string Message { get; set; }
+
+public bool Success { get; set; }
+public dynamic Data { get; set; }
+    }
+}
---
+++ MedCore/MedCore.Domain/Repository/IBaseReporsitory.cs
@@ -0,0 +1,26 @@
+﻿using MedCore.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedCore.Domain.Repository
+{
+    public interface IBaseReporsitory <TEntity, TType> where TEntity : class 
+    {
+        Task<TEntity> GetEntityByIdAsync(TType id); 
+Task UpdateEntityAsync(TEntity entity);
+
+Task DeleteEntityAsync(TEntity entity);
+        Task SaveEntityAsync(TEntity entity);
+        Task<List<TEntity>> GetAllAsync();
+
+        Task<OperationResult> GetAll(Expression<Func<TEntity, bool>> filter);
+
+        Task<OperationResult> GetEntityBy(int Id);
+
+        Task<bool> Exists(Expression<Func<TEntity, bool>> filter);
+    }
+}
