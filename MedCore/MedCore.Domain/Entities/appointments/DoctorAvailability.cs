dacf485e7bb8dfbfc9791c1eb3563b3f6c04e107
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-07 22:43:15 -0400

    DoctorAvailability

---
+++ MedCore/MedCore.Domain/Entities/appointments/DoctorAvailability.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedCore.Domain.Entities.appointments
+{
+    public sealed class DoctorAvailability : Base.BaseEntity<int>
+    {
+        [Column("AvailabilityID")]
+        [Key]
+        public override int Id { get; set; }
+        public DateTime AvailableDate { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly? EndTime { get; set; }
+    }
+}
