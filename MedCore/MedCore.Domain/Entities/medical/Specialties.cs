c442d1d7f6c72c2e22ecb875e2e1b6068e873c03
rurbaez <rurbaez90@gmail.com>
2025-02-08 00:19:49 -0400

    more changes

---
+++ MedCore/MedCore.Domain/Entities/medical/Specialties.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using MedCore.Domain.Base;
+
+namespace MedCore.Domain.Entities
+{
+    public sealed class Specialties : Base.BaseEntity<short>
+
+    {
+        [Column("SpecialtyID")]
+        [Key]
+        public override short Id { get; set; }
+        public required string SpecialtyName { get; set; }
+    }
+    
+}
---
+++ MedCore/MedCore.Domain/Entities/medical/availabilityModes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MedCore.Domain.Entities.medical
+
+{
+    public sealed class AvailabilityModes : Base.BaseEntity<short>
+    {
+        [Column("SAvailabilityModelID")]
+        [Key]
+        public override short Id { get; set; }
+        public required string AvailabilityMode { get; set; }
+       
+    }
+}
