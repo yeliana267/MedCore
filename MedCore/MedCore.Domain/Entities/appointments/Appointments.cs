d488a1fb93ba6121b1612be274ce6ae6647e5289
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-07 22:22:20 -0400

    Appoiments

--- MedCore/MedCore.Domain/Entities/appointments/Appointments.cs
+++ MedCore/MedCore.Domain/Entities/appointments/Appointments.cs
@@ -1,8 +1,18 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MedCore.Domain.Entities.appointments
 {
-  public sealed class Appointments : Base.BaseEntity
+    public sealed class Appointments : Base.BaseEntity<int>
     {
+        [Column("AppointmentID")]
+        [Key]
+        public override int Id { get; set; }
+        public int PatientID { get; set; }
+        public int DoctorID { get; set; }
+        public int StatusID { get; set; }
+        public DateTime AppointmentDate { get; set; }
 
     }
 }
