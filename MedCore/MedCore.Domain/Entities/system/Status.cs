646a84365e476f5c7bfd930a5effcc82010b1f17
randytejeda1211 <randytejeda1211@gmail.com>
2025-02-07 21:40:33 -0400

    Creacion de entidades de system

---
+++ MedCore/MedCore.Domain/Entities/system/Status.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace MedCore.Domain.Entities.system
+{
+    internal sealed class Status
+    {
+        public int StatusID { get; set; }
+        public string StatusName { get; set; }
+    }
+}
