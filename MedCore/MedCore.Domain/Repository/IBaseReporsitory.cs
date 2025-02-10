a68e3e43638a383eba6779631f3bece92ce46d6a
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-10 04:16:42 -0400

    Persistence

--- MedCore/MedCore.Domain/Repository/IBaseReporsitory.cs
+++ MedCore/MedCore.Domain/Repository/IBaseReporsitory.cs
@@ -4,18 +4,15 @@ using System.Linq.Expressions;
 
 namespace MedCore.Domain.Repository
 {
-    public interface IBaseReporsitory <TEntity, TType> where TEntity : class 
+    public interface IBaseReporsitory<TEntity, TType> where TEntity : class 
     {
         Task<TEntity> GetEntityByIdAsync(TType id); 
-        Task UpdateEntityAsync(TEntity entity);
+        Task<OperationResult> UpdateEntityAsync(TEntity entity);
 
-        Task DeleteEntityAsync(TEntity entity);
-        Task SaveEntityAsync(TEntity entity);
+        Task<OperationResult> SaveEntityAsync(TEntity entity);
         Task<List<TEntity>> GetAllAsync();
 
         Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter);
-
-        Task<OperationResult> GetEntityByAsync(int Id);
 
         Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter);
     }
--- MedCore/MedCore.Persistence/Base/BaseRepository.cs
+++ MedCore/MedCore.Persistence/Base/BaseRepository.cs
@@ -1,7 +1,90 @@
 ﻿
+using MedCore.Domain.Base;
+using MedCore.Domain.Repository;
+using MedCore.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
 namespace MedCore.Persistence.Base
 {
-     public abstract class BaseRepository<TEntity, TType> 
+    public abstract class BaseRepository<TEntity, TType> : IBaseReporsitory<TEntity, TType> where TEntity : class
     {
+        private readonly MedCoreContext _context;
+        private DbSet<TEntity> Entity { get; set; }
+
+        protected BaseRepository(MedCoreContext context)
+        {
+            _context = context;
+            this.Entity = _context.Set<TEntity>();
+        }
+        public virtual async Task<TEntity> GetEntityByIdAsync(TType id)
+        {
+            return await Entity.FindAsync(id);
+        }
+
+        public virtual async Task<OperationResult> UpdateEntityAsync(TEntity entity)
+        {
+            OperationResult result = new OperationResult();
+
+            try
+            {
+                Entity.Update(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Message = $"Ocurrió un error {ex.Message} actualizando la entidad.";
+
+            }
+
+            return result;
+        }
+        public virtual async Task<OperationResult> SaveEntityAsync(TEntity entity)
+        {
+            OperationResult result = new OperationResult();
+            try
+            {
+                Entity.Add(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.Message = $"Ocurrió un error {ex.Message} guardando la entidad.";
+
+            }
+
+            return result;
+        }
+        public virtual async Task<List<TEntity>> GetAllAsync()
+        {
+            return await Entity.ToListAsync();
+        }
+
+        public virtual async Task<OperationResult> GetAllAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            OperationResult result = new OperationResult();
+
+            try
+            {
+                var datos = await this.Entity.Where(filter).ToListAsync();
+                result.Data = datos;
+            }
+            catch (Exception ex)
+            {
+
+                result.Success = false;
+                result.Message = $"Ocurrió un error {ex.Message} obteniendo los datos.";
+            }
+
+            return result;
+
+        }
+
+        public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await Entity.AnyAsync(filter);
+        }
     }
 }
