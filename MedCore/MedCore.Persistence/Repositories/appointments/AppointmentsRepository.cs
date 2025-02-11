e2dcb03405ff54d715d582a1b049e95b1d14e5dc
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-10 23:43:21 -0400

    Cambio

---
+++ MedCore/MedCore.Persistence/Repositories/appointments/AppointmentsRepository.cs
@@ -0,0 +1,17 @@
+﻿
+
+using MedCore.Domain.Entities.appointments;
+using MedCore.Persistence.Base;
+using MedCore.Persistence.Context;
+using MedCore.Persistence.Interfaces.appointments;
+
+namespace MedCore.Persistence.Repositories.appointments
+{
+    public class AppointmentsRepository : BaseRepository<Appointments, int>, IAppointmentsRepository
+    {
+        public AppointmentsRepository(MedCoreContext context) : base(context)
+        {
+        }
+    }
+
+}
