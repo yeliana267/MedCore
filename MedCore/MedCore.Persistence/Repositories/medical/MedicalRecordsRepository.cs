cd938aad757545f647b6f4ca06e3ed67683ac8f9
rurbaez <rurbaez90@gmail.com>
2025-02-12 00:52:38 -0400

    Creacion de medical repositorys(persistence)

---
+++ MedCore/MedCore.Persistence/Repositories/medical/MedicalRecordsRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using MedCore.Domain.Entities.medical;
+using MedCore.Persistence.Base;
+using MedCore.Persistence.Context;
+using MedCore.Persistence.Interfaces.medical;
+
+namespace MedCore.Persistence.Repositories.medical
+{
+    public class MedicalRecordsRepository : BaseRepository<MedicalRecords, int>, IMedicalRecordsRepository
+    {
+        public MedicalRecordsRepository(MedCoreContext context) : base(context)
+        {
+        }
+    }
+}
