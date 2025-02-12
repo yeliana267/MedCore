62f35c0f11258ec0124651dc40f5d39f7212c925
randytejeda1211 <randytejeda1211@gmail.com>
2025-02-11 23:23:17 -0400

    MedCoreContext (System) Creacion de "DbSet"

--- MedCore/MedCore.Persistence/Context/MedCoreContext.cs
+++ MedCore/MedCore.Persistence/Context/MedCoreContext.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using MedCore.Domain.Entities.appointments;
+using MedCore.Domain.Entities.system;
 using Microsoft.EntityFrameworkCore;
 
 namespace MedCore.Persistence.Context
@@ -12,5 +13,9 @@ namespace MedCore.Persistence.Context
         }
         public DbSet<Appointments> Appointments { get; set; }
         public DbSet<DoctorAvailability> DoctorAvailabilities { get; set; }
+
+        public DbSet<Notifications> Notifications { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Status> Status { get; set; }
     }
 }
