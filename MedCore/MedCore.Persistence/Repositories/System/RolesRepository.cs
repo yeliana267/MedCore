8856143fd06efd0ec7a66401d19d803cc462f85e
randytejeda1211 <randytejeda1211@gmail.com>
2025-02-11 23:35:33 -0400

    Creacion de repositorios (System)

---
+++ MedCore/MedCore.Persistence/Repositories/System/RolesRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using MedCore.Domain.Entities.system;
+using MedCore.Persistence.Base;
+using MedCore.Persistence.Context;
+using MedCore.Persistence.Interfaces.System;
+
+namespace MedCore.Persistence.Repositories.System
+{
+    public class RolesRepository : BaseRepository<Roles, int>, IRolesRepository
+    {
+        public RolesRepository(MedCoreContext context) : base(context) 
+        {
+        }
+    }
+}
