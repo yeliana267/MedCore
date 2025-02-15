659726db040ec2f23731e0e5cc13e49981732269
mariana <marianaemiliasanchez24@gmail.com>
2025-02-15 17:01:06 -0400

    validaciones init

--- MedCore/MedCore.Persistence/Repositories/Insurance/NetworkTypeRepository.cs
+++ MedCore/MedCore.Persistence/Repositories/Insurance/NetworkTypeRepository.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using MedCore.Domain.Base;
 using MedCore.Domain.Entities.Insurance;
 using MedCore.Persistence.Base;
 using MedCore.Persistence.Context;
@@ -9,9 +10,26 @@ namespace MedCore.Persistence.Repositories.Insurance
 {
     public class NetworkTypeRepository : BaseRepository<NetworkType, int>, INetworkTypeRepository
     {
+        private readonly MedCoreContext context;
         public NetworkTypeRepository(MedCoreContext context) : base(context)
         {
+            this.context = context;
 
+        }
+
+        public override Task<OperationResult> SaveEntityAsync(NetworkType entity)
+        {
+
+            /// validaciones
+          
+
+
+            return base.SaveEntityAsync(entity);
+        }
+
+        public override Task<OperationResult> UpdateEntityAsync(NetworkType entity)
+        {
+            return base.UpdateEntityAsync(entity);
         }
     }
 }
