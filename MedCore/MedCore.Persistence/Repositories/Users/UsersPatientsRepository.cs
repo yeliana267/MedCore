51f1079774643f4730a79065fc1e011761c4c089
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-15 16:48:48 -0400

    Repository

--- MedCore/MedCore.Persistence/Repositories/Users/UsersPatientsRepository.cs
+++ MedCore/MedCore.Persistence/Repositories/Users/UsersPatientsRepository.cs
@@ -1,7 +1,15 @@
 ﻿
+using MedCore.Domain.Entities.Users;
+using MedCore.Persistence.Base;
+using MedCore.Persistence.Context;
+using MedCore.Persistence.Interfaces.Users;
+
 namespace MedCore.Persistence.Repositories.Users
 {
-    public class UsersPatientsRepository
+    public class UsersPatientsRepository : BaseRepository<UsersPatients, int>, IUsersPatientsRepository
     {
+        public UsersPatientsRepository(MedCoreContext context) : base(context) 
+        { 
+        }
     }
 }
