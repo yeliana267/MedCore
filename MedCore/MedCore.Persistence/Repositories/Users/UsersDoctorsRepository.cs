8985f777c9089d997a193234cb617f9f4ce8ac75
AlvelyBR <alvelybencosmeramos@gmail.com>
2025-02-15 21:55:19 -0400

    Repository

--- MedCore/MedCore.Persistence/Repositories/Users/UsersDoctorsRepository.cs
+++ MedCore/MedCore.Persistence/Repositories/Users/UsersDoctorsRepository.cs
@@ -1,17 +1,26 @@
 ﻿
-
-using MedCore.Domain.Entities.Users;
-using MedCore.Persistence.Base;
-using MedCore.Persistence.Context;
-using MedCore.Persistence.Interfaces.Users;
+    using MedCore.Domain.Base;
+    using MedCore.Domain.Entities.Users;
+    using MedCore.Persistence.Base;
+    using MedCore.Persistence.Context;
+    using MedCore.Persistence.Interfaces.Users;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedCore.Persistence.Repositories.Users
 {
+
     public class UsersDoctorsRepository : BaseRepository<UsersDoctors, int>, IUsersDoctorsRepository
     {
+        private readonly MedCoreContext context;
         public UsersDoctorsRepository(MedCoreContext context) : base(context)
         {
+            this.context = context;
+        }
+        public override Task<OperationResult> SaveEntityAsync(UsersDoctors entity)
+        {
+           
 
+            return base.SaveEntityAsync(entity);
         }
     }
 }
