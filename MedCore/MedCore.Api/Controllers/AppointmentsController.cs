c80c001bf6e3456ba39aad1832323e2c8579b73f
Yeliana Diaz <nanadiaz267@gmail.com>
2025-02-22 19:04:15 -0400

    cambio

---
+++ MedCore/MedCore.Api/Controllers/AppointmentsController.cs
@@ -0,0 +1,54 @@
+﻿using MedCore.Persistence.Interfaces.appointments;
+using MedCore.Persistence.Repositories.appointments;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MedCore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        public readonly IAppointmentsRepository _appointmentsRepository;
+        public readonly ILogger<AppointmentsController> _logger;
+        public readonly IConfiguration _configuration;
+        public AppointmentsController(IAppointmentsRepository  appointmentsRepository, ILogger<AppointmentsController> logger, IConfiguration configuration) { 
+            _appointmentsRepository = appointmentsRepository; 
+            _logger = logger;
+            _configuration = configuration;
+        }
+        // GET: api/<AppointmentsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var appointments = await _appointmentsRepository.GetAllAsync();
+            return Ok();
+        }
+
+        // GET api/<AppointmentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AppointmentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AppointmentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AppointmentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
