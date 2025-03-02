55138fce55399c1eaf175f73121cb72fe82cfc3d
rurbaez <rurbaez90@gmail.com>
2025-03-02 03:20:55 -0400

    creacion dtos medical

---
+++ MedCore/MedCore.Application/Dtos/Medical/AvailibilityModesDto/UpdateAvailibilityModesDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MedCore.Application.Dtos.Medical.AvailibilityModesDto
+{
+    public class UpdateAvailibilityModesDto
+    {
+        public short Id { get; set; }
+        
+    }
+}
---
+++ MedCore/MedCore.Application/Dtos/Medical/MedicalRecordsDto/UpdateMedicalRecordsDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MedCore.Application.Dtos.Medical.MedicalRecordsDto
+{
+    public class UpdateMedicalRecordsDto : MedicalRecordsDto
+    {
+        public int Id { get; set; }
+        
+    }
+}
