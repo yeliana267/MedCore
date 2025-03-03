e28c67c03434ef0ba747e3fd5990c35931829401
rurbaez <rurbaez90@gmail.com>
2025-03-02 23:41:58 -0400

    creacion medical IOC

---
+++ MedCore/MedCore.IOC/Dependencies/Medical/MedicalRecordsDependency.cs
@@ -0,0 +1,17 @@
+﻿using MedCore.Application.Interfaces.Medical;
+using MedCore.Application.Services.Medical;
+using MedCore.Persistence.Interfaces.medical;
+using MedCore.Persistence.Repositories.medical;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MedCore.IOC.Dependencies.Medical
+{
+    public class MedicalRecordsDependency
+    {
+        public static void AddMedicalRecordsDependency(IServiceCollection services)
+        {
+            services.AddScoped<IMedicalRecordsRepository, MedicalRecordsRepository>();
+            services.AddScoped<IMedicalRecordsService, MedicalRecordsService>();
+        }
+    }
+}
