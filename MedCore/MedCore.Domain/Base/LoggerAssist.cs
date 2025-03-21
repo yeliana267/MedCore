2b48246e2cdb635f18f8875e8dc198ff1095cb1c
Yeliana Diaz <nanadiaz267@gmail.com>
2025-03-21 00:16:22 -0400

    Cambios de borrar

---
+++ MedCore/MedCore.Domain/Base/LoggerAssist.cs
@@ -0,0 +1,37 @@
+﻿using MedCore.Domain.Repository;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace MedCore.Domain.Base
+{
+    public class LoggerAssist<T>: ILoggerAssist<T> where T : class
+    {
+        private static readonly ILogger _logger = Log.Logger;
+
+        public void Info(string mensaje)
+        {
+            _logger.Information(mensaje);
+        }
+
+        public void Advertencia(string mensaje)
+        {
+            _logger.Warning(mensaje);
+        }
+
+        public void Error(string mensaje, Exception ex)
+        {
+            _logger.Error(ex, mensaje);
+        }
+
+        public void Error(string mensaje)
+        {
+            _logger.Error(mensaje);
+            throw new Exception(mensaje);
+        }
+
+        public void Info(string mensaje, Exception ex)
+        {
+           _logger.Information(mensaje, ex);
+        }
+    }
+}
---
+++ MedCore/MedCore.Domain/Repository/ILoggerAssist.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MedCore.Domain.Repository
+{
+    public interface ILoggerAssist<T> where T : class
+    {
+        void Info(string mensaje);
+        void Info(string mensaje,Exception ex);
+
+        void Advertencia(string mensaje);
+        void Error(string mensaje, Exception ex);
+        void Error(string mensaje);
+    }
+}
